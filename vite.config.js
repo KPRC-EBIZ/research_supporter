884e53a6fdbae84b7eb84d262f70c52b4d871441
LEEYEJI0839 <foretell59@kprc.or.kr>
2026-07-03 16:58:41 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/priceresearchsupporter/",
+  base: "/research_supporter/",
   plugins: [react()],
   resolve: {
     dedupe: ["react", "react-dom"],
